1faf8ad5c64ab0427b272dddff942d06adb39906
Shilpi <sinshilpi20@gmail.com>
2016-09-14 00:36:34 +0530

    test commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def index
+    @content_first = 'This is a sample text for my awesome new Ruby Blog';
+    @content_second = 'This is more sample text for awesome new Ruby Blog';
+  end
+  def new
+    
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
+  def show
+    
+  end
+  def destroy
     
   end
 end
