20ae39611b5a7363837983d1be2db8eaf44f4553
Shilpi <sinshilpi20@gmail.com>
2016-09-17 22:00:49 +0530

    dynamic forms for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   def new
-    
+    @post = Post.new
+    @category = Category.all
   end
   def create
-    
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => "Your post has been saved"
+    else
+      render "new"
+    end
   end
   def edit
     
